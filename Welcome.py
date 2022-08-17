6bac98a973aa126a8a16c06654d2c93aeaa15567
Emad DARWICH <e.a.darwich@gmail.com>
2022-08-17 13:12:11 +0200

    add main

---
+++ Welcome.py
@@ -0,0 +1,34 @@
+# https://blog.streamlit.io/introducing-multipage-apps/
+
+# Contents of ~/my_app/streamlit_app.py
+import streamlit as st
+
+def main_page():
+    st.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Main page 🎈")
+
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
+}
+
+#selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+#page_names_to_funcs[selected_page]()
+
+
+
+st.markdown("<h1 style='text-align: center; color: black;'>Welcome to Swift!</h1>", unsafe_allow_html=True)
+st.markdown("""
+Swift combines the best of `C` and `Objective-C`.
+
+It's ideal for creating new apps and works seamlessly with `Objective-C`, so it's compatible with apps previously created in that language.
+""")
