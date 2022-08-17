da03e55d493a14ea01a11bc3cf63252f8b4093b6
Emad DARWICH <e.a.darwich@gmail.com>
2022-08-17 13:11:12 +0200

    add pages

---
+++ pages/Lesson4.py
@@ -0,0 +1,95 @@
+
+# Contents of ~/my_app/streamlit_app.py
+from cProfile import label
+import streamlit as st
+
+st.set_page_config(
+    page_icon=":blue_heart:",
+)
+
+
+st.markdown("<h1 style='text-align: center; color: black;'>Lesson 4</h1>", unsafe_allow_html=True)
+
+st.markdown("""
+# Basic Operators
+
+An **operator** is a special symbol or phrase used to check, change, or combine values.
+
+Operators are unary, binary, or ternary:
+- **Unary** Operator: Has a single target (-a). A unary prefix operator is placed before the target (!b).
+- **Binary** Operator: Has two targets (4 + 5) and is infixed, appearing between the two targets.
+- **Ternary** Operator: Has three targets. Like C, **_Swift has one ternary operator_**, the ternary conditional operator (a ? b : c).
+
+The values targeted by operators are called operands. In the expression 1 + 2, the + symbol is a binary operator; its two operands are the values 1 and 2.
+""")
+code = """
+var WelcomeMsg : String
+WelcomeMsg = "Hello"
+"""
+
+st.code(code, language='swift')
+
+
+
+st.markdown("""
+This example provides a type annotation for a variable called "welcomeMsg", that indicates that the variable can store **String** values.
+
+
+It's possible to define multiple related variables of the same type. Include them all on a single line, separated by commas, then add a single type annotation after the final variable name:
+""")
+code = """
+a = 88
+var red, green, blue: Double
+"""
+
+st.code(code, language='swift')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+st.markdown("""
+In practice, you will rarely need to add type annotations. Providing an initial value for a constant or a variable at the point at which it is defined, will almost always be sufficient for Swift to infer which type should be used:
+""")
+code = """
+let pi = 3.14159 // Double
+"""
+
+st.code(code, language='swift')
+
+
+
+st.markdown("""
+Swift always chooses Double (as opposed to Float) when inferring the type for floating-point numbers.
+
+Swift is a **type safe** language, meaning that it supports clarity when specifying value types for code. When part of your code expects a String, you can't pass it an Int by mistake.
+""")
+
